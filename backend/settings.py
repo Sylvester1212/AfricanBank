ef3f0689076921d9576a2e97ed7b6ab6333ccba9
Marshall-007 <marshalldube9@gmail.com>
2025-03-27 17:55:31 +0200

    Updated Title, Discription and icon to Afribit.

--- backend/settings.py
+++ backend/settings.py
@@ -41,11 +41,11 @@ class _UiSettings(BaseSettings):
         env_ignore_empty=True
     )
 
-    title: str = "African Bank"
+    title: str = "AFRIBIT"
     logo: Optional[str] = None
     chat_logo: Optional[str] = None
     chat_title: str = "Financial Assistant"
-    chat_description: str = " AI-powered by African Bank."
+    chat_description: str = " AI-powered by AFRIBIT."
     favicon: str = "/favicon.ico"
     show_share_button: bool = True
     show_chat_history_button: bool = True
